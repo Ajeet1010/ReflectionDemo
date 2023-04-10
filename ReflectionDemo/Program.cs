c94736d697086f6675be7f0ce4727dc2ece9e40e
Ajeet <ajeetpal101999@gmail.com>
2023-04-11 01:40:26 +0530

    [Ajeet] Reflection Demo using created or another project.

--- ReflectionDemo/Program.cs
+++ ReflectionDemo/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace ReflectionDemo
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ReflectionDemo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("*****  Reflection Demo  *****");
+            ReflectionTest.Test();
+
+            string path = @"D:\Visual Studio\Assignment\CollectionProblem\CollectionProblem\bin\Debug\CollectionProblem.dll";
+
+            Assembly assembly = Assembly.LoadFile(path);
+
+            Type[] types = assembly.GetTypes();
+            foreach(var type  in types)
+            {
+                Console.WriteLine("Class: " + type.Name);
+
+                MethodInfo[] methods = type.GetMethods();
+                foreach(MethodInfo method in methods)
+                {
+                    Console.WriteLine("--Method:  " + method.Name);
+                    ParameterInfo[] parameters = method.GetParameters();
+                    foreach(ParameterInfo param in parameters)
+                    {
+                        Console.WriteLine("---- Parameters:  " + param.Name + ":" + param.ParameterType);
+                    }
+                }
+            }
         }
     }
 }
