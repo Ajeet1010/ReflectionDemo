0bb3d2ec3102cc5f643b678ef003ac0f60e3a893
Ajeet <ajeetpal101999@gmail.com>
2023-04-11 00:25:51 +0530

    [Ajeet] Reflection demo.

---
+++ ReflectionDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ReflectionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****  Reflection Demo  *****");
+        }
+    }
+}
